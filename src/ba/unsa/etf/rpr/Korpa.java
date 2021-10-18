61d1d2eddab171bcb466e4b785a44c7276b7408d
amarhasanovic <ahasanovic4@etf.unsa.ba>
2021-10-18 13:35:42 +0200

    #

--- src/ba/unsa/etf/rpr/Korpa.java
+++ src/ba/unsa/etf/rpr/Korpa.java
@@ -3,5 +3,21 @@ package ba.unsa.etf.rpr;
 public class Korpa {
     private Artikl[] korpa = new Artikl[50];
 
+    public boolean dodajArtikl(Artikl artikl){
+        for(int i=0; i<50; i++){
+            if(korpa[i] == null){
+                korpa[i] = artikl;
+                break;
+            }
+        }
+        if(korpa.length<=50)
+            return true;
+        return false;
+    }
 
+    public Artikl[] getArtikli(){ return korpa; }
+
+    public Artikl izbaciArtiklSaKodom(String kod){
+
+    }
 }
